c5ebf91a79104f68a55a86e390ccc149097516cf
Eduardo <eduardo.conti@gazin.com.br>
2022-05-04 14:51:47 -0300

    correção ssl

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { HttpExceptionFilter } from '@shared/exceptions';
 import { TransformInterceptor, ValidationPipe } from '@shared/pipes';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
