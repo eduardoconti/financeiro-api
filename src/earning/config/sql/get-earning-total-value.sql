e61552459b5715173743a97247772bea108385e8
Eduardo <eduardo.conti@gazin.com.br>
2022-08-23 22:10:21 -0300

    fix: fix sql to considerate timestamp

--- src/earning/config/sql/get-earning-total-value.sql
+++ src/earning/config/sql/get-earning-total-value.sql
@@ -1,24 +1,30 @@
 SELECT
-    SUM(r.valor) as total,
-    SUM(
-        CASE
-            WHEN r.pago = false THEN r.valor
-        END
+    coalesce(SUM(r.valor), 0) as total,
+    coalesce(
+        SUM(
+            CASE
+                WHEN r.pago = false THEN r.valor
+            END
+        ),
+        0
     ) as "totalOpen",
-    SUM(
-        CASE
-            WHEN r.pago = true THEN r.valor
-        END
+    coalesce(
+        SUM(
+            CASE
+                WHEN r.pago = true THEN r.valor
+            END
+        ),
+        0
     ) as "totalPayed"
 FROM
     receitas r
 WHERE
     r.user_id = $1
     AND CASE
-        WHEN (cast($2 as date)) IS NOT NULL
-        AND (cast($3 as date)) IS NOT NULL THEN r.pagamento BETWEEN $2
+        WHEN (cast($2 as timestamp)) IS NOT NULL
+        AND (cast($3 as timestamp)) IS NOT NULL THEN r.pagamento BETWEEN $2
         AND $3
-        WHEN(cast($2 as date)) IS NOT NULL
-        AND (cast($3 as date)) IS NULL THEN r.pagamento >= $2
+        WHEN(cast($2 as timestamp)) IS NOT NULL
+        AND (cast($3 as timestamp)) IS NULL THEN r.pagamento >= $2
         ELSE true
     END
--- src/expense/config/sql/get-expense-total-value.sql
+++ src/expense/config/sql/get-expense-total-value.sql
@@ -1,24 +1,30 @@
 SELECT
-    SUM(d.valor) as total,
-    SUM(
-        CASE
-            WHEN d.pago = false THEN d.valor
-        END
+    coalesce(SUM(d.valor), 0) as total,
+    coalesce(
+        SUM(
+            CASE
+                WHEN d.pago = false THEN d.valor
+            END
+        ),
+        0
     ) as "totalOpen",
-    SUM(
-        CASE
-            WHEN d.pago = true THEN d.valor
-        END
+    coalesce(
+        SUM(
+            CASE
+                WHEN d.pago = true THEN d.valor
+            END
+        ),
+        0
     ) as "totalPayed"
 FROM
     despesas d
 WHERE
     d.user_id = $1
     AND CASE
-        WHEN (cast($2 as date)) IS NOT NULL
-        AND (cast($3 as date)) IS NOT NULL THEN d.vencimento BETWEEN $2
+        WHEN (cast($2 as timestamp)) IS NOT NULL
+        AND (cast($3 as timestamp)) IS NOT NULL THEN d.vencimento BETWEEN $2
         AND $3
-        WHEN(cast($2 as date)) IS NOT NULL
-        AND (cast($3 as date)) IS NULL THEN d.vencimento >= $2
+        WHEN(cast($2 as timestamp)) IS NOT NULL
+        AND (cast($3 as timestamp)) IS NULL THEN d.vencimento >= $2
         ELSE true
     END
