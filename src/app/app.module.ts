f9dd7f302dcce3c26ab546397dd480c1117258d2
Eduardo Conti <es.eduardoconti@gmail.com>
2023-12-15 23:46:33 -0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AuthModule } from '@auth/auth.module';
 
@@ -45,19 +45,24 @@ import { AppService } from './service';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: configService.get('DATABASE_SYNCHRONIZE') ?? false,
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        logging:  configService.get('DATABASE_LOGGING') ?? false,
-        ssl: { rejectUnauthorized: false },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const config: any = {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: configService.get('DATABASE_SYNCHRONIZE') ?? false,
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          logging:  configService.get('DATABASE_LOGGING') ?? false,
+        }
+
+        if(configService.get('NODE_ENV') === 'production'){
+          config.ssl = { rejectUnauthorized: false }
+        }
+        return config},
     }),
   ],
   controllers: [AppController],
